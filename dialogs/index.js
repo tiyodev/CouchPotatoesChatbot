a6be78ab243dedd41dfebf14dc4536ef35053fb1
titiyo <bondeau.yohann@gmail.com>
2016-06-30 16:05:53 +0200

    nouveau LUIS

--- dialogs/index.js
+++ dialogs/index.js
@@ -1,14 +1,58 @@
 var builder = require('botbuilder');
+var message = require('./message');
 
-var model = process.env.model || 'https://api.projectoxford.ai/luis/v1/application?id=28bafb7d-f89e-49dc-8fda-498d0c20ce5a&subscription-key=7992140f378349d5be19042f01bc09f3&q=';
+var model = process.env.model || 'https://api.projectoxford.ai/luis/v1/application?id=ce9081fa-e36d-44cb-a510-76bcb9035bd7&subscription-key=c44b73c3cbcd4060b7be0844b4044f6d&q=';
 var dialog = new builder.LuisDialog(model);
 module.exports = dialog;
 
-dialog.onDefault(builder.DialogAction.send("Moi pas comprendre toi !"));
+//dialog.onDefault(builder.DialogAction.send("Moi pas comprendre toi !"));
 
-/** Prompts a user for the title of the task and saves it.  */
 dialog.on('Welcome', [
     function (session, args) {
-        session.send("Bien le bonjour de la part de Couch Potatoes Chat Bot !");
+        session.send(message.welcomeMsg, session.message.from.name);
+    }
+]);
+
+
+dialog.on('Search', [
+    function (session, args, next) {
+        //test si la personne à précisé le type de contenu qu'elle souhaite regarder
+        var typeOfContent = builder.EntityRecognizer.findEntity(args.entities, 'TypeOfContent');
+        var moment = builder.EntityRecognizer.findEntity(args.entities, 'Moment');
+
+        var search = session.dialogData.search = {
+            typeOfContent: typeOfContent ? typeOfContent.entity : null,
+            moment: moment ? moment.entity : null  
+        };
+
+        // Si le type de contenu n'est pas présent, demander une précision à l'utilisateur
+        if(!typeOfContent){
+            builder.Prompts.text(session, message.typeOfContent);
+        }
+        else{
+            next();
+        }
+    },
+    function (session, results, next) {
+        var search = session.dialogData.search;
+        if (results.response) {
+            search.typeOfContent = results.response;
+        }
+
+        // Si le moment n'est pas présent, demander une précision à l'utilisateur
+        if (search.typeOfContent && !search.moment) {
+            builder.Prompts.time(session, message.moment);
+        } else {
+            next();
+        }
+    },
+    function (session, results) {
+        var search = session.dialogData.search;
+        if (results.response) {
+            var time = builder.EntityRecognizer.resolveTime([results.response]);
+            search.moment = time ? time : null;
+        }
+
+        session.send(message.searchResume, search.typeOfContent, search.moment);
     }
 ]);
---
+++ dialogs/message.js
@@ -0,0 +1,6 @@
+module.exports = {
+    welcomeMsg : 'Bonjour %s, je suis le Bot Couch Potatoes. Si tu recherches une émission passée ou a venir : je peux t\'aider.',
+    typeOfContent : 'Quel type de contenu veux tu regarder ?',
+    moment : 'A quel moment veux tu regarder ton contenu ?',
+    searchResume : 'Patiente un petit peu, je recherche %s pour %s.'
+}
