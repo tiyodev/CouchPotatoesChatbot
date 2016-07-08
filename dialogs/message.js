62da27aa455fcbaea6083c9db6ac05710c7bd57b
titiyo <bondeau.yohann@gmail.com>
2016-07-08 19:01:54 +0200

    Correction des messages

--- dialogs/message.js
+++ dialogs/message.js
@@ -1,5 +1,5 @@
 module.exports = {
-    welcomeMsg : 'Bonjour %s, je suis le chatbot Couch Potatoes ! Que puis-je faire pour toi ?,
+    welcomeMsg : 'Bonjour %s, je suis le chatbot Couch Potatoes ! Que puis-je faire pour toi ?',
     typeOfContent : 'Que veux-tu regarder ?',
     moment : 'A quel moment veux tu le regarder ?',
     searchResume : 'Une petite seconde, je recherche %s pour %s.'
