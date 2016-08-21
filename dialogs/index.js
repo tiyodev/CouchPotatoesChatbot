4762988a66996c12952d945a415e4a0bbe72b1e5
tiyo <tiyodev@gmail.com>
2016-08-21 19:58:12 +0200

    test API ARTE

--- dialogs/index.js
+++ dialogs/index.js
@@ -1,5 +1,6 @@
 var builder = require('botbuilder');
 var message = require('./message');
+var http = require('http');
 
 var model = process.env.model || 'https://api.projectoxford.ai/luis/v1/application?id=a19fd36f-ec91-4e0d-9a28-492cf56936e4&subscription-key=c44b73c3cbcd4060b7be0844b4044f6d&q=';
 var dialog = new builder.LuisDialog(model);
@@ -7,9 +8,27 @@ module.exports = dialog;
 
 //dialog.onDefault(builder.DialogAction.send("Moi pas comprendre toi !"));
 
+var options = {
+    host: 'https://api.arte.tv/api/opa/status',
+    port: '80',
+    method: 'POST',
+    headers: {
+        'Authorization': 'Bearer NjhlOGRiM2UzZTU4ZmU5ZWFmM2UxNGFhNGY4Yjc2MGFmZWVjYTc1NmE5Y2U4YTI5OTIyMzA1YzQ1ZWZhOWExNw',
+        'Accept' : 'application/vnd.api+json; version=2.1'
+    }
+};
+
+var req = http.request(options, function(res) {
+    console.write(res);
+});
+
 dialog.on('Welcome', [
     function (session, args) {
         session.send(message.welcomeMsg, session.message.from.name);
+
+        //req.write('post=data&is=specified&like=this');
+        req.write();
+        req.end();
     }
 ]);
 
