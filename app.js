dd9440241d4610029cffe1c9ef2a0f6bd96089d5
titiyo <bondeau.yohann@gmail.com>
2016-08-22 15:58:55 +0200

    Mise à jour de la version de l'api BotBuilder de MS

--- app.js
+++ app.js
@@ -2,13 +2,43 @@ var restify = require('restify');
 var builder = require('botbuilder');
 var index = require('./dialogs/index')
 
+//=========================================================
+// Bot Setup
+//=========================================================
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url);
+});
+
+// Create chat bot
+var connector = new builder.ChatConnector({
+    appId: 'c1b9c7e2-d311-48e4-8ad4-67e9c14583f2',
+    appPassword: 'ydP2SmeAO5Gm5TiefEDGm16'
+});
+var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
+
+//=========================================================
+// Bots Dialogs
+//=========================================================
+
+bot.dialog('/', index);
+
+
+
+
+
+/*
 // Create bot and add dialogs
-var bot = new builder.BotConnectorBot({ appId: '12345678900987654321', appSecret: 'ab87d13b80f642cea7ed25d934497dd9' });
-bot.add('/', index);
+var bot = new builder.UniversalBot({ appId: '12345678900987654321', appSecret: 'ab87d13b80f642cea7ed25d934497dd9' });
+bot.dialog('/', index);
 
 // Setup Restify Server
 var server = restify.createServer();
 server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
 server.listen(process.env.port || 3978, function () {
-   console.log('%s listening to %s', server.name, server.url); 
+   console.log('%s listening to %s', server.name, server.url);
 });
+*/
--- dialogs/index.js
+++ dialogs/index.js
@@ -2,26 +2,27 @@ var builder = require('botbuilder');
 var message = require('./message');
 var http = require('http');
 var request = require('request');
+var moment = require('moment');
 
 var model = process.env.model || 'https://api.projectoxford.ai/luis/v1/application?id=a19fd36f-ec91-4e0d-9a28-492cf56936e4&subscription-key=c44b73c3cbcd4060b7be0844b4044f6d&q=';
 //var model = process.env.model || 'https://api.projectoxford.ai/luis/v1/application?id=5d6ff76e-8dbe-466c-9daf-493701814eef&subscription-key=7992140f378349d5be19042f01bc09f3&q='
-var dialog = new builder.LuisDialog(model);
+var recognizer = new builder.LuisRecognizer(model);
+var dialog = new builder.IntentDialog({ recognizers: [recognizer] });
+
 module.exports = dialog;
 
-
-dialog.onDefault( 
+//dialog.onDefault(
    // console.log(new Date(Date.UTC(16, 08, 22, 0, 0, 0)))
-);
+//);
 
-
-dialog.on('Welcome', [
+dialog.matches('Welcome', [
     function (session, args) {
-        session.send(message.welcomeMsg, session.message.from.name);
+       session.send(message.welcomeMsg, session.message.address.user.name);
     }
 ]);
 
 
-dialog.on('Search', [
+dialog.matches('Search', [
     function (session, args, next) {
         //test si la personne à précisé le type de contenu qu'elle souhaite regarder
         var typeOfContent = builder.EntityRecognizer.findEntity(args.entities, 'TypeOfContent');
@@ -29,7 +30,7 @@ dialog.on('Search', [
 
         var search = session.dialogData.search = {
             typeOfContent: typeOfContent ? typeOfContent.entity : null,
-            moment: moment ? moment.entity : null  
+            moment: moment ? moment.entity : null
         };
         // Si le type de contenu n'est pas présent, demander une précision à l'utilisateur
         if(!typeOfContent){
@@ -53,79 +54,62 @@ dialog.on('Search', [
         }
     },
     function (session, results) {
-        console.log("Data : ");
+      /*  console.log("Data : ");
         console.log(session.dialogData);
-        console.log("\n\n");
+        console.log("\n\n"); */
         var search = session.dialogData.search;
-        console.log("Result : ");
+      /*  console.log("Result : ");
         console.log(results);
         console.log("\n\n");
 
         console.log("Search : ");
         console.log(search);
-        console.log("\n\n");
+        console.log("\n\n");*/
 
-       // if (results.response) {
-            //var moment = builder.EntityRecognizer.findEntity(results.response, 'Moment');
-            //console.log(moment);
-            //search.moment = moment ? moment.entity : null;
+        var dateRequest = moment();
 
-            //console.log("YBO 1");
+        if(search.moment = "demain")
+        {
+            dateRequest = dateRequest.add(1, 'days');
+        }
 
-            search.moment = results.response;
+        session.send(message.searchResume, search.typeOfContent, search.moment);
 
-           // console.log("YBO 2");
+        var options = {
+             url: 'https://api.arte.tv/api/opa/v2/search/broadcastProgrammings',
+             headers: {
+                 'Authorization': 'Bearer NjhlOGRiM2UzZTU4ZmU5ZWFmM2UxNGFhNGY4Yjc2MGFmZWVjYTc1NmE5Y2U4YTI5OTIyMzA1YzQ1ZWZhOWExNw',
+                 'Accept' : 'application/vnd.api+json; version=2.32'
+             },
+             qs: {
+                 'channel' : 'FR',
+                 'arteSchedulingDay' : dateRequest.toISOString().substring(0, 11),
+                 'limit' : '3'
+             }
+         };
 
-            session.send(message.searchResume, search.typeOfContent, search.moment);
+        request(options, function (error, response, body) {
+              if (!error && response.statusCode == 200) {
+                  var data = JSON.parse(body);
 
-           // console.log("YBO 3");
+                  for(var i = 0; i < data.broadcastProgrammings.length; i++)
+                  {
+                      var card = new builder.HeroCard(session)
+                                .title(data.broadcastProgrammings[i].program.title)
+                                .subtitle(data.broadcastProgrammings[i].program.subtitle)
+                                .images([
+                                    builder.CardImage.create(session, data.broadcastProgrammings[i].program.mainImage.url)
+                                ])
+                                .buttons([builder.CardAction.openUrl(session, data.broadcastProgrammings[i].program.url, 'Détail')])
 
-           var test = new Date().toISOString(); 
-          // var dateDemain;
-
-           // if(search.moment = 'demain')
-           // {
-              //  console.log(test);
-
-              //  var jourSemaine = test.getYear();
-
-              //  console.log(test.getYear());
-               // console.log(test.getMonth());
-               // console.log(test.getDay());
-              //  dateDemain = new Date(Date.UTC(test.getYear(), test.getMonth(), test.getDay() + 1, 0, 0, 0));
-              //  console.log(dateDemain);
-            //}
-
-          //  session.send("Erreur durant la recherche !");
-
-         /*   var options = {
-                url: 'https://api.arte.tv/api/opa/v2/schedule/after/2016-08-16T00:00:00Z',
-                headers: {
-                    'Authorization': 'Bearer NjhlOGRiM2UzZTU4ZmU5ZWFmM2UxNGFhNGY4Yjc2MGFmZWVjYTc1NmE5Y2U4YTI5OTIyMzA1YzQ1ZWZhOWExNw',
-                    'Accept' : 'application/vnd.api+json; version=2.1'
-                },
-                qs: {
-                    'language' : 'fr',
-                    'country' : 'FR',
-                    'limit' : '3'
-                }
-            };
-
-            console.log("YBO 4");
-
-            //'broadcastProgrammings.broadcastBegin' : new Date(Date.UTC(16, 08, 22, 0, 0, 0))
-
-            request(options, function (error, response, body) {
-                if (!error && response.statusCode == 200) {
-                    console.log("YBO 5");
-                    console.log(body) // Show the HTML for the Google homepage.
-                }
-                else
-                {
-                    console.log("YBO 6");
-                    session.send("Erreur durant la recherche !");
-                }
-            })*/
-        //}
+                       var msg = new builder.Message(session).attachments([card]);
+                       session.send(msg);
+                  }
+              }
+              else
+              {
+                  session.send("Erreur durant la recherche !");
+              }
+          })
     }
 ]);
